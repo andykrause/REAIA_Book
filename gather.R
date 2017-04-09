21bb380b7a17c8cb69f6d97c725c2be4719865c7
Andy Krause <andy.krause@unimelb.edu.au>
2017-04-08 20:51:28 -0700

    Cosmetic Code changes

--- gather.R
+++ gather.R
@@ -1,105 +1,117 @@
+##########################################################################################
+#                                                                                        #            
+#  Code for Chapter 4 (Gather) of Real Estate Analysis in the Information Age            #   
+#                                                                                        #                  
+##########################################################################################
+ 
+### Set up directories -------------------------------------------------------------------
 
  ## Set your data directory
 
-  #data.dir <- 'c:/dropbox/research/bigdatabook/data/'
   data.dir <- 'c:/temp/'
 
-## Set up a capture location for downloaded files
+ ## Set up a capture location for downloaded files
   
   # Create a directory if one doesn't exist
-  if(!dir.exists(file.path(data.dir, 'raw_zip_files'))){
+  if (!dir.exists(file.path(data.dir, 'raw_zip_files'))){
     dir.create(file.path(data.dir, 'raw_zip_files'))
   }
   
- ## Download and unzip the Seattle Police Beat Shapefile
+### Gather and extract the Seattle Police Beat Shapefile ---------------------------------
 
-  # Check if file exists, download and unzip if it doesn't
-  if(!file.exists(file.path(data.dir, 'raw_zip_files', 'beats.zip'))){
+  # Check if file exists
+  if (!file.exists(file.path(data.dir, 'raw_zip_files', 'beats.zip'))){
   
-    # Download
+    # If it doesn't, then download
     download.file(url=paste0('https://data.seattle.gov/views/nnxn-434b/files/',
                          '96d998d4-ae20-4ea8-b912-436e68982a0d.zip'), 
                   destfile=file.path(data.dir, 'raw_zip_files', 'beats.zip'))
   }  
 
-  # Create a directory if one doesn't exist
-  if(!dir.exists(file.path(data.dir, 'beats'))){
+  # Create a directory for beats data if one doesn't exist
+  if (!dir.exists(file.path(data.dir, 'beats'))){
     dir.create(file.path(data.dir, 'beats'))
   }
   
   # Unzip the files
-  unzip(file.path(data.dir, 'raw_zip_files', 'beats.zip'),
+  unzip(zipfile=file.path(data.dir, 'raw_zip_files', 'beats.zip'),
         exdir=file.path(data.dir, 'beats'))
         
- ## Download and unzip the King County parcel shapefile
+### Gather and extract the King County Parcel Shapefile ----------------------------------
   
-  # Create a directory if one doesn't exist
-  if(!dir.exists(file.path(data.dir, 'geographic'))){
-    dir.create(file.path(data.dir, 'geographic'))
-  }
-  
-  # Check if file exists, download and unzip if it doesn't
+  # Check if file exists
   if(!file.exists(file.path(data.dir, 'raw_zip_files', 'parcel_shapefile.zip'))){
     
-    # Download
+    # If it doesn't, then download
     download.file(url=paste0('ftp://ftp.kingcounty.gov/gis-web/web/',
                              'GISData/parcel_SHP.zip'), 
                   destfile=file.path(data.dir, 'raw_zip_files', 'parcel_shapefile.zip'))
   }
-  
-  # Unzip
-  unzip(file.path(data.dir, 'raw_zip_files', 'parcel_shapefile.zip'), 
-        exdir=file.path(data.dir, 'geographic'))
-  
-## Download and unzip the King County Assessor's Data Files
 
   # Create a directory if one doesn't exist
-  if(!dir.exists(file.path(data.dir, 'assessor'))){
-    dir.create(file.path(data.dir, 'assessor'))
+  if (!dir.exists(file.path(data.dir, 'geographic'))){
+    dir.create(file.path(data.dir, 'geographic'))
   }
-
-  # Check if file exists, download and unzip if it doesn't
-  if(!file.exists(file.path(data.dir, 'raw_zip_files', 'sales.zip'))){
+  
+  # Unzip the files
+  unzip(zipfile=file.path(data.dir, 'raw_zip_files', 'parcel_shapefile.zip'), 
+        exdir=file.path(data.dir, 'geographic'))
+  
+### Gather and extract the King County Assessor Data Files -------------------------------
+  
+ ## Sales Transaction File  
+  
+  # Check if file exists
+  if (!file.exists(file.path(data.dir, 'raw_zip_files', 'sales.zip'))){
     
-  ## Sales File  
-    # Download
+    # If it doesn't, then download
     download.file(url=paste0('http://your.kingcounty.gov/extranet/assessor/',
                              'Real Property Sales.zip'), 
                   destfile=file.path(data.dir, 'raw_zip_files', 'sales.zip'))
   }
   
+  # Create a directory if one doesn't exist
+  if (!dir.exists(file.path(data.dir, 'assessor'))){
+    dir.create(file.path(data.dir, 'assessor'))
+  }
+  
   # Unzip
-  unzip(file.path(data.dir, 'raw_zip_files', 'sales.zip'), 
+  unzip(zipfile=file.path(data.dir, 'raw_zip_files', 'sales.zip'), 
         exdir=file.path(data.dir, 'assessor'))
 
-  # Check if file exists, download and unzip if it doesn't
-  if(!file.exists(file.path(data.dir, 'raw_zip.files', 'parcel.zip'))){
+ ## Parcel (land) information file  
+  
+  # Check if file exists
+  if (!file.exists(file.path(data.dir, 'raw_zip.files', 'parcel.zip'))){
     
-  ## Parcel File
-    # Download
+    # If it doesn't, then download
     download.file(url='http://your.kingcounty.gov/extranet/assessor/Parcel.zip', 
                   destfile=file.path(data.dir, 'raw_zip_files', 'parcel.zip'))
   }
   
   # Unzip
-  unzip(file.path(data.dir, 'raw_zip_files', 'parcel.zip'), 
-                  exdir=file.path(data.dir, 'assessor'))
+  unzip(zipfile=file.path(data.dir, 'raw_zip_files', 'parcel.zip'), 
+        exdir=file.path(data.dir, 'assessor'))
   
-  # Check if file exists, download and unzip if it doesn't
-  if(!file.exists(file.path(data.dir, 'raw_zip_files', 'resbldg.zip'))){
+ ## Residential Building File  
+  
+  # Check if file exists
+  if (!file.exists(file.path(data.dir, 'raw_zip_files', 'resbldg.zip'))){
     
-  ## ResBldg
-    # Download
-    download.file(url='http://your.kingcounty.gov/extranet/assessor/Residential Building.zip', 
+    # If it doesn't, then download
+    download.file(url=paste0('http://your.kingcounty.gov/extranet/assessor/',
+                             'Residential Building.zip'), 
                   destfile=file.path(data.dir, 'raw_zip_files', 'resbldg.zip'))
   }
     
   # Unzip
-  unzip(file.path(data.dir, 'raw_zip_files', 'resbldg.zip'), 
-                  exdir=file.path(data.dir, 'assessor'))
+  unzip(zipfile=file.path(data.dir, 'raw_zip_files', 'resbldg.zip'), 
+        exdir=file.path(data.dir, 'assessor'))
 
+  
 ### SAVE FOR PROCESS TO DOWNLOAD NICK'S TWITTER DATA -------------------------------------  
   
   
   
-
+##########################################################################################
+##########################################################################################
